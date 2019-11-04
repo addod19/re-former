4108c2c616a07ca88d9dd8ffffe176891217d976
macnick <nick@macexperts.gr>
2019-11-04 17:32:57 +0200

    Work for extra credit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  def a_validation_method
+    errors.add(:username, "Username must be a least 10 characters long, please correct.")
+  end
   validates :username, presence: true, length: { minimum: 10 }
-  validates :email, presence: true, length: { minimum: 20 }
+  validates :email, presence: true, length: { minimum: 12 }
   validates :password, presence: true
 end
