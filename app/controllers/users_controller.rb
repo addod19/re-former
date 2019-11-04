562434893b3bea6e9c39bccb84d9456f8883ebf4
macnick <nick@macexperts.gr>
2019-11-04 13:12:25 +0200

    HTML form 6 - 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user)
+    user = {
+      username: params[:username],
+      email: params[:email],
+      password: params[:password]
+    }
   end
 end
