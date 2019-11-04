9a3cd987594c3dc3a4feaeaaa0570148ecc42a75
stickler-ci <support@stickler-ci.com>
2019-11-04 16:12:42 +0000

    Fixing style errors.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params) #(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params) # (username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -28,6 +30,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   def a_validation_method
-    errors.add(:username, "Username must be a least 10 characters long, please correct.")
+    errors.add(:username, 'Username must be a least 10 characters long, please correct.')
   end
   validates :username, presence: true, length: { minimum: 10 }
   validates :email, presence: true, length: { minimum: 12 }
