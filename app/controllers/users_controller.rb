8ab4026888ff38e033c38b9e7876c243adf0401a
addod19 <addodaniellarbi@gmail.com>
2019-11-07 13:37:38 +0000

    optional comment deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # (username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
