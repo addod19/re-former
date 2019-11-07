5a45ef52d264acb67a1da5f5176a2104ce808b41
addod19 <addodaniellarbi@gmail.com>
2019-11-07 13:35:26 +0000

    before merging comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :username, presence: { message: ' must not be blank.' }, length: { minimum: 10 }
-  validates :email, presence: true, length: { minimum: 12 }
+  validates :username, uniqueness: true, presence: { message: ' must not be blank.' }, length: { minimum: 10 }
+  validates :email, uniqueness: true, presence: true, length: { minimum: 12 }
   validates :password, presence: true
 end
